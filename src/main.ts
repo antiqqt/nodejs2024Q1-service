dcb63b1c99331471629cdafd3119fb45c4c286e7
Anton Pomogaev <antonpomogaev@gmail.com>
2024-03-09 20:35:31 +0300

    feat: add env with ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') ?? 4000;
+  console.log(port)
+  await app.listen(port);
 }
 bootstrap();
